045135a949d7ff9e5144bc7c21dc8505865f55b7
Hui Kong <kong.291@osu.edu>
2018-11-26 20:51:20 -0800

    more plots

--- sys-analysis/systest.py
+++ sys-analysis/systest.py
@@ -167,15 +167,15 @@ Obiwansys(eboss,'hppsfsize_r',sysmax=2,sysmin=0.9,dataset = 'uniform',xlab='psf 
 Obiwansys(eboss,'hppsfsize_r',sysmax=2,sysmin=0.9,dataset = 'obiwan',xlab = 'psf size r')
 '''
 
-'''
+
 Obiwansys(eboss,'hpstardens',sysmax=1300,sysmin=900,dataset = 'uniform',xlab='star density')
 Obiwansys(eboss,'hpstardens',sysmax=1300,sysmin=900,dataset = 'obiwan',xlab='star density')
+
+
 '''
-
-
 Obiwansys(eboss,'hpebv',sysmax=0.085,sysmin=0.01,dataset = 'uniform',xlab='extinction')
 Obiwansys(eboss,'hpebv',sysmax=0.085,sysmin=0.01,dataset = 'obiwan',xlab='extinction')
-
+'''
 
 '''
 Obiwansys(eboss,'hppsfdepth_r',sysmax=600,sysmin=80,dataset = 'uniform',xlab='psf depth r')
